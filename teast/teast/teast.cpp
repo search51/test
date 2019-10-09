0bf2b2d5695dc57c3bee70694a03cbec5241192a
search51 <56347602+search51@users.noreply.github.com>
2019-10-09 14:25:18 +0200

    feasd

---
+++ teast/teast/teast.cpp
@@ -0,0 +1,11 @@
+// teast.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
+//
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
+
